1ef04a312fbd66276060d449e881ac9ffd7ad7ef
Sriharsha Polimetla <sriharsha186@gmail.com>
2020-08-03 20:19:56 +0200

    enabled integration tests for all

--- tests.sh
+++ tests.sh
@@ -5,21 +5,17 @@ currentFolder=$(pwd)
 cd $currentFolder/src/functions
 
 errorCode=0
-# for d in */; do
-#         cd "$d"
-#         echo "Processing $d"
+for d in */; do
+        cd "$d"
+        echo "Processing $d"
 
-#         # build, run linter and unit tests
-#         npm install
-#         npm run integration
-#         if [[ $? -ne 0 ]]; then
-#             errorCode=1
-#         fi
-#         cd ..
-# done
-
-cd list-devices
-npm install
-npm run integration
+        # build, run linter and unit tests
+        npm install
+        npm run integration
+        if [[ $? -ne 0 ]]; then
+            errorCode=1
+        fi
+        cd ..
+done
 cd $currentFolder
 exit $errorCode
