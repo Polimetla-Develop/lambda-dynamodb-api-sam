46083ffa463a9f55dac1f4e400a82425df2e2ab4
Sriharsha Polimetla <sriharsha186@gmail.com>
2020-08-03 20:11:47 +0200

    clean up

--- src/functions/list-devices/tests/integration/tests.js
+++ src/functions/list-devices/tests/integration/tests.js
@@ -19,6 +19,7 @@ describe('Test GET Devices API', function () {
         .end(function (err, res, body) {
           if (err) throw err;
           expect(body).to.be.an('array');
+          console.log(body);
           done();
         });
     } catch (err) {
@@ -26,7 +27,7 @@ describe('Test GET Devices API', function () {
     }
   });
 
-  xit('verifies response for no api key', (done) => {
+  it('verifies response for no api key', (done) => {
     try {
       hippie()
         .get(url)
@@ -38,86 +39,6 @@ describe('Test GET Devices API', function () {
         });
     } catch (err) {
       done(err);
-    }
-  });
-
-  xit('verifies response for authorization error', (done) => {
-    try {
-      hippie()
-        .get(url)
-        .header('x-api-key', apiKey)
-        .header('User-Agent', userAgent)
-        .expectStatus(401)
-        .end(function (err, res, body) {
-          if (err) throw err;
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
-  });
-
-  xit('verifies response for POST method not defined', (done) => {
-    try {
-      hippie()
-        .post(url)
-        .header('x-api-key', apiKey)
-        .header('User-Agent', userAgent)
-        .end(function (err, res, body) {
-          if (err) throw err;
-          done();
-        });
-    } catch (err) {
-      expect(err.message).to.contains('Swagger spec does not define method: "post" in path');
-      done();
-    }
-  });
-
-  xit('verifies response for PUT method not defined', (done) => {
-    try {
-      hippie()
-        .put(url)
-        .header('x-api-key', apiKey)
-        .header('User-Agent', userAgent)
-        .end(function (err, res, body) {
-          if (err) throw err;
-          done();
-        });
-    } catch (err) {
-      expect(err.message).to.contains('Swagger spec does not define method: "put" in path');
-      done();
-    }
-  });
-
-  xit('verifies response for DELETE method not defined', (done) => {
-    try {
-      hippie()
-        .del(url)
-        .header('x-api-key', apiKey)
-        .header('User-Agent', userAgent)
-        .end(function (err, res, body) {
-          if (err) throw err;
-          done();
-        });
-    } catch (err) {
-      expect(err.message).to.contains('Swagger spec does not define method: "delete" in path');
-      done();
-    }
-  });
-
-  xit('verifies response for invalid path', (done) => {
-    try {
-      hippie()
-        .del(host + '/content/something')
-        .header('x-api-key', apiKey)
-        .header('User-Agent', userAgent)
-        .end(function (err, res, body) {
-          if (err) throw err;
-          done();
-        });
-    } catch (err) {
-      expect(err.message).to.contains('Swagger spec does not define path');
-      done();
     }
   });
 });
