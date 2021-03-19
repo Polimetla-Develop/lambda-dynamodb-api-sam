53b2a3ded12ebcf0e5516284e11b113f6bf8cdc6
Sriharsha Polimetla <38424697+S-Polimetla@users.noreply.github.com>
2021-03-19 14:05:48 +0100

    Update index.js

--- src/functions/get-device/index.js
+++ src/functions/get-device/index.js
@@ -1,38 +1,11 @@
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.TABLE;
-const fs = require('fs-extra');
+const req = require('./test.js');
 
-exports.handler = async (event) => {
-  const deviceId = event.pathParameters.deviceId;
+exports.handler = async (event) => {  
   try {
-    const deviceInfo = await this.loadDevice(deviceId);
-    if (!deviceInfo) {
-      return {
-        statusCode: 404
-      };
-    }
-    return {
-      statusCode: 200,
-      body: JSON.stringify(deviceInfo)
-    };
+    console.log('calling test')
+    const mydata = req.test();
+    console.log(mydata);
   } catch (err) {
     console.log(err);
-    return {
-      statusCode: 500
-    };
   }
-};
-
-exports.loadDevice = async (deviceId) => {
-  const params = {
-    TableName: tableName,
-    Key: {
-      deviceId: deviceId
-    }
-  };
-
-  const device = await docClient.get(params).promise();
-  if (!device.Item || !device.Item.deviceInfo) return null;
-  return device.Item.deviceInfo;
 };
